2d2723846b2eb3e85ba9af51d8c8717666440f56
mdYaqin <abu_naal@hotmail.com>
2021-10-20 15:18:45 +0800

    setting up navbar components

---
+++ event-app/src/features/nav/NavBar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Button, Container, Menu } from 'semantic-ui-react'
+
+const NavBar = () => {
+  return (
+    <Menu inverted fix='top'>
+      <Container>
+        <Menu.Item header>
+          <img src='/assets/logo.png' alt='logo' style={{ marginRight: 15 }} />
+          Event App
+        </Menu.Item>
+        <Menu.Item name='Events' />
+        <Menu.Item>
+          <Button positive inverted content='Create Event' />
+        </Menu.Item>
+        <Menu.Item position='right'>
+          <Button basic inverted content='Login' />
+          <Button
+            basic
+            inverted
+            content='Register'
+            style={{ marginLeft: '0.5em' }}
+          />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  )
+}
+
+export default NavBar
