af57e55a8f885d866b3e44fa75c79586ede8f1ae
mdYaqin <abu_naal@hotmail.com>
2021-10-22 18:08:00 +0800

    added sampleData to seed to homepage to be used as props

---
+++ event-app/src/app/api/sampleData.js
@@ -0,0 +1,50 @@
+export const sampleData = [
+  {
+    id: '1',
+    title: 'Trip to Wellington Circle',
+    date: '2018-03-21',
+    category: 'culture',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'NY, USA',
+    venue: 'Empire State Building, 5th Avenue, New York, NY, USA',
+    hostedBy: 'Atok',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+      },
+      {
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+      },
+    ],
+  },
+  {
+    id: '2',
+    title: 'Trip to NUH',
+    date: '2018-03-18',
+    category: 'Checkup',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'Singapore',
+    venue: 'NUH Singapore',
+    hostedBy: 'Dr Vanaja',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+      },
+      {
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+      },
+    ],
+  },
+]
--- event-app/src/features/events/eventDashboard/EventDashboard.jsx
+++ event-app/src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react'
+import EventForm from '../eventForm/EventForm'
 import EventList from './EventList'
+import { sampleData } from '../../../app/api/sampleData'
 
 const EventDashboard = () => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList />
+        <EventList events={sampleData} />
       </Grid.Column>
       <Grid.Column width={6}>
-        <h2>Right Column</h2>
+        <EventForm />
       </Grid.Column>
     </Grid>
   )
