a25a351628857c28253264d8919a895a357af41c
mdYaqin <abu_naal@hotmail.com>
2021-10-28 20:21:27 +0800

    added reducer

---
+++ event-app/src/features/events/eventActions.js
@@ -0,0 +1,20 @@
+import { CREATE_EVENT } from './eventConstants'
+
+export function createEvent(event) {
+  return {
+    type: CREATE_EVENT,
+    payload: event,
+  }
+}
+export function createEvent(event) {
+  return {
+    type: CREATE_EVENT,
+    payload: event,
+  }
+}
+export function createEvent(event) {
+  return {
+    type: CREATE_EVENT,
+    payload: event,
+  }
+}
