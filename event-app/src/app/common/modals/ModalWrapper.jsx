642fd8ae876ade464bd0b43407b0ca026d66c85a
mdYaqin <abu_naal@hotmail.com>
2021-11-10 13:50:56 +0800

    added modal manager

---
+++ event-app/src/app/common/modals/ModalWrapper.jsx
@@ -0,0 +1,15 @@
+import React, { useDispatch } from 'react'
+import { Modal } from 'semantic-ui-react'
+import { closeModal } from './modalReducer'
+
+const ModalWrapper = ({ children, size, header }) => {
+  const dispatch = useDispatch()
+  return (
+    <Modal open={true} onClose={() => dispatch(closeModal())} size={size}>
+      {header && <Modal.Header>{header}</Modal.Header>}
+      <Modal.Content>{children}</Modal.Content>
+    </Modal>
+  )
+}
+
+export default ModalWrapper
--- event-app/src/features/sandbox/Sandbox.jsx
+++ event-app/src/features/sandbox/Sandbox.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button } from 'semantic-ui-react'
+import { openModal } from '../../app/common/modals/modalReducer'
 import { decrement, increment } from './testReducer'
 
 const Sandbox = () => {
@@ -19,6 +20,11 @@ const Sandbox = () => {
         onClick={() => dispatch(decrement(10))}
         content='Decrement'
         color='red'
+      />
+      <Button
+        onClick={() => dispatch(openModal())}
+        content='Open Modal'
+        color='teal'
       />
     </>
   )
